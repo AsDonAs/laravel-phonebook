7db56af8b7ac70eb65d1c8fafb081c19f7148841
Kupreev D <zomgwwh@yandex.ru>
2021-02-13 15:53:39 +0300

    Added PhoneContact migration.

---
+++ database/migrations/2021_02_13_123945_create_phone_contacts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhoneContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phone_contacts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('first_name')->nullable(false);
+            $table->string('second_name')->nullable(true)->default(NULL);
+            $table->string('last_name')->nullable(true)->default(NULL);
+            $table->string('phone')->nullable(true)->default(NULL);
+            $table->boolean('is_favorite')->nullable(false)->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('phone_contacts', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+        Schema::dropIfExists('phone_contacts');
+    }
+}
