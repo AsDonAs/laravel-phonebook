39a64a1ddafe7969d5c65be1d8c816ba41375d52
Kupreev D <zomgwwh@yandex.ru>
2021-02-14 22:06:47 +0300

    Fix buttons view.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
             @auth
                 <div class="row">
                     <div class="col-md-6">
-                        <a href="{{ route('phone-contacts') }}" class="btn btn-success" title="{{ __('Phone contacts') }}">
+                        <a href="{{ route('phone-contacts') }}" class="btn btn-success d-flex align-items-center" title="{{ __('Phone contacts') }}">
                             <span class="material-icons">list_alt</span>
                             {{ __('Phone contacts') }}
                         </a>
@@ -17,7 +17,7 @@
                         <form class="form-inline" method="POST" action="{{ route('logout') }}">
                             @csrf
                             <x-nav-link :href="route('logout')"
-                                        class="btn btn-danger"
+                                        class="btn btn-danger d-flex align-items-center"
                                         title="{{ __('Logout') }}"
                                         onclick="event.preventDefault();
                             this.closest('form').submit();">
@@ -30,12 +30,12 @@
                 </div>
             @else
                 @if (Route::has('register'))
-                    <a href="{{ route('register') }}" class="btn btn-info" title="{{ __('Register') }}">
+                    <a href="{{ route('register') }}" class="btn btn-info d-flex align-items-center" title="{{ __('Register') }}">
                         <span class="material-icons">person_add_alt_1</span>
                         {{ __('Register') }}
                     </a>
                 @endif
-                <a href="{{ route('login') }}" class="btn btn-success" title="{{ __('Login') }}">
+                <a href="{{ route('login') }}" class="btn btn-success d-flex align-items-center" title="{{ __('Login') }}">
                     <span class="material-icons">login</span>
                     {{ __('Login') }}
                 </a>
